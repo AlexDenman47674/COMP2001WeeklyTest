575d312c0dd3693788a9366ef3a904d45f399fd3
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2020-11-22 14:53:32 +0000

    Added Radio Buttons

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+include_once('../public/header.php');
+include_once('../public/footer.php');
+?>
+    <body class="bg-info">
+    <div class="container-fluid col-md-10 offset-md-1">
+        <div class="row">
+            <div class="card mt-3 px-2 py-2">
+                <h1>Stationary Application</h1>
+                <p>Welcome to this fictional stationary application page created for COMP2001. To continue please accept our terms and conditions</p>
+                <div>
+                    <input type="radio" id="Agree" name="accept?" value=True>
+                    <label for="Agree">Agree</label><br>
+                    <input type="radio" id="Disagree" name="accept?" value=False>
+                    <label for="Disagree">Disagree</label>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
