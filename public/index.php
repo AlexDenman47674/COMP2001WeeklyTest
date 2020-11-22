d0ca5e424c9fe6e90ce742da2a814e498032775f
AlexDenman47674 <57488236+AlexDenman47674@users.noreply.github.com>
2020-11-22 14:16:17 +0000

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+include_once('../public/header.php');
+include_once('../public/footer.php');
+?>
+    <body class="bg-info">
+    <div class="container-fluid col-md-10 offset-md-1">
+        <div class="row">
+            <div class="card mt-3 px-2 py-2">
+                <h1>Stationary Application</h1>
+            </div>
+        </div>
+    </div>
+    </body>
